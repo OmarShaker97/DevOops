4efc56286efe3565355735763acb90a02ad3bad5
OmarShaker97 <34036869+OmarShaker97@users.noreply.github.com>
2020-12-29 11:45:28 +0200

    Feat: added monitoring trade aggregator process & proxy

--- DevOops.py
+++ DevOops.py
@@ -14,34 +14,70 @@ fileOpener.close()
 currentTradeAggregatorServersState = dict()
 previousTradeAggregatorServersState = dict()
 
+# Initializing Trade Aggregator Process State
+currentTradeAggregatorProcessState = dict()
+previousTradeAggregatorProcessState = dict()
+
 # Initializing Order Proxy Service State
 currentOrderProxyServiceState = dict()
 previousOrderProxyServiceState = dict()
+
+# Initializing Trade Aggregator Proxy Process State
+currentTradeAggregatorProxyProcessState = dict()
+previousTradeAggregatorProxyProcessState = dict()
+
+# Initializing Trade Aggregator Proxy State
+currentTradeAggregatorProxyPortState = dict()
+previousTradeAggregatorProxyPortState = dict()
 
 # Trade Aggregator Servers are initially up and running
 for (key, value) in data['trade-aggregator-servers'].items():
     currentTradeAggregatorServersState[key] = True
     previousTradeAggregatorServersState[key] = True
 
+# Trade Aggregator Processes are initially up and running
+for (key, value) in data['trade-aggregator-process'].items():
+    currentTradeAggregatorProcessState[key] = True
+    previousTradeAggregatorProcessState[key] = True
+
 # Order Proxy Services are initially up and running
 for (key, value) in data['order-proxy-service'].items():
     currentOrderProxyServiceState[key] = True
     previousOrderProxyServiceState[key] = True
 
+# Trade Aggregator Processes are initially up and running
+for (key, value) in data['trade-aggregator-proxy'].items():
+    currentTradeAggregatorProxyProcessState[key] = True
+    previousTradeAggregatorProxyProcessState[key] = True
+    currentTradeAggregatorProxyPortState[key] = True
+    previousTradeAggregatorProxyPortState[key] = True
+
+
 # Monitor Trade Aggregator Servers
 
 
-def monitorTradeAggregatorServers():
+def monitorTradeAggregatorServers(mode):
     # Loop Over Servers
     for (key, value) in data['trade-aggregator-servers'].items():
         # Run netstat command
-        cmd = 'netstat -an | findstr %s | findstr LISTENING' % (
-            value['port'])  # CHANGE LISTENING TO ESTABLISHED
+        cmd = 'netstat -an | findstr %s | findstr %s' % (
+            value['port'], mode)  # mode = ESTABLISHED
 
         output = os.popen(cmd).read()
         # Check Trade Aggregator Servers State
         checkIfPortExists(key, value, output, currentTradeAggregatorServersState,
                           previousTradeAggregatorServersState)
+
+
+# Monitor Trade Aggregator Processes
+
+
+def monitorTradeAggregatorProcess():
+    # Loop Over Processes
+    for (key, value) in data['trade-aggregator-process'].items():
+        # Check if each process is running
+        checkIfProcessRunning(
+            key, value['name'], currentTradeAggregatorProcessState, previousTradeAggregatorProcessState)
 
 # Monitor Order Proxy Services
 
@@ -52,14 +88,25 @@ def monitorOrderProxyService():
         # Check if each process is running
         checkIfProcessRunning(
             key, value['name'], currentOrderProxyServiceState, previousOrderProxyServiceState)
-        if(previousOrderProxyServiceState[key] and not currentOrderProxyServiceState[key]):
-            previousOrderProxyServiceState[key] = False
-            print('Order Proxy Service %s went from up to down! Sending Email..' %
-                  value['name'])
-        elif(currentOrderProxyServiceState[key] and not previousOrderProxyServiceState[key]):
-            previousOrderProxyServiceState[key] = True
-            print('Order Proxy Service %s went from down to up! Sending Email..' %
-                  value['name'])
+
+# Monitor Trade Aggregator Proxy Processes
+
+
+def monitorTradeAggregatorProxy(mode):
+    # Loop Over Services
+    for (key, value) in data['trade-aggregator-proxy'].items():
+        # Check if each process is running
+        checkIfProcessRunning(
+            key, value['name'], currentTradeAggregatorProxyProcessState, previousTradeAggregatorProxyProcessState)
+
+        # Run netstat command
+        cmd = 'netstat -an | findstr %s | findstr %s' % (
+            value['port'], mode)  # mode = LISTENING
+
+        output = os.popen(cmd).read()
+        # Check Trade Aggregator Servers State
+        checkIfPortExists(key, value, output, currentTradeAggregatorProxyPortState,
+                          previousTradeAggregatorProxyPortState)
 
 
 def checkIfPortExists(key, value, output, currentState, previousState):
@@ -72,13 +119,44 @@ def checkIfPortExists(key, value, output, currentState, previousState):
         # If the IP Address is found in the output of the command, then server is running
         if(value['ip-address'] in output):
             currentState[key] = True
-            print('Found!')
+            print('IP Address: %s was found!' % value['ip-address'])
             break
         # Otherwise, the server is not running, thus its current state is set to False
         else:
             currentState[key] = False
-            print('Not Found')
+            print('IP Address: %s was not found!' % value['ip-address'])
             attempts += 1
+    checkAndUpdateState(attempts, key, previousState, currentState)
+
+
+def checkIfProcessRunning(key, processName, currentState, previousState):
+    # Attempts initially is set to zero
+    attempts = 0
+    # Sleep for a given time before retrying
+    while(attempts < data['retryAttempts']):
+        time.sleep(data['waitTime'])
+        # Iterate over the all the running process
+        for proc in psutil.process_iter():
+            try:
+                # Check if process name contains the given name string.
+                if processName.lower() in proc.name().lower():
+                    print('Process %s was found!' % processName)
+                    currentState[key] = True
+                    checkAndUpdateState(
+                        attempts, key, previousState, currentState)
+                    return
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                print('Process %s was not found!' % processName)
+                currentState[key] = False
+                attempts += 1
+                break
+        print('Process %s was not found!' % processName)
+        currentState[key] = False
+        attempts += 1
+    checkAndUpdateState(attempts, key, previousState, currentState)
+
+
+def checkAndUpdateState(attempts, key, previousState, currentState):
     # If the number of attempts to ping the server surpasses retryAttempts from config file and the server was previously
     # running then change previous state of the server from True to False and sent email to DevOps team to notify them
     if(attempts >= data['retryAttempts'] and previousState[key]
@@ -90,26 +168,6 @@ def checkIfPortExists(key, value, output, currentState, previousState):
     elif(currentState[key] and not previousState[key]):
         previousState[key] = True
         print('Sending Email (Went from down to up)')
-
-
-def checkIfProcessRunning(key, processName, currentState, previousState):
-    # Sleep for a given time before retrying
-    time.sleep(data['waitTime'])
-    # Iterate over the all the running process
-    for proc in psutil.process_iter():
-        try:
-            # Check if process name contains the given name string.
-            if processName.lower() in proc.name().lower():
-                print('%s was found!' % processName)
-                currentState[key] = True
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            print('%s was not found!' % processName)
-            currentState[key] = False
-            return False
-    print('%s was not found!' % processName)
-    currentState[key] = False
-    return False
 
 
 def runInParallel(*fns):
@@ -124,4 +182,7 @@ def runInParallel(*fns):
 
 if __name__ == '__main__':
     while(True):
+        monitorTradeAggregatorServers("ESTABLISHED")
+        monitorTradeAggregatorProcess()
         monitorOrderProxyService()
+        monitorTradeAggregatorProxy("LISTENING")
